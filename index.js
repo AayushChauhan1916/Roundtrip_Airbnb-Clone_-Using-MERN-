46365b97869612a06d53a43d5fe81690512485ba
Aayush Chauhan <aayushchauhan1916@gmail.com>
2024-01-31 22:02:50 +0530

    fixing some b212g

--- index.js
+++ index.js
@@ -1,17 +1,102 @@
-if(process.env.NODE_ENV != "production"){
-    require('dotenv').config()
+// if(process.env.NODE_ENV != "production"){
+//     require('dotenv').config()
+// }
+// const express = require("express");
+// const app  = express();
+// const mongoose = require('mongoose');
+// const path = require("path");
+// const methodOverride = require("method-override");
+// const ejsMate = require("ejs-mate");
+// const ExpressError = require("./utils/ExpressErrors.js");
+
+
+// const listingsRoute = require("./routes/listing.js");
+// const reviewsRoute = require("./routes/review.js");
+
+// const session = require("express-session");
+// const MongoStore = require('connect-mongo');
+// const flash = require("connect-flash");
+// const passport = require("passport");
+// const LocalStrategy = require("passport-local");
+// const User = require("./Models/user.js");
+// const userRoute = require("./routes/user.js")
+// const mongoUrl = process.env.ATLASDB_URL;
+
+// app.set("view engine","ejs");
+// app.set("views",path.join(__dirname,"/views"));
+// app.use(express.urlencoded({extended:true}));
+// app.use(methodOverride("_method"));
+// app.engine("ejs",ejsMate);
+// app.use(express.static(path.join(__dirname,"/public")));
+
+// main()
+//     .then(()=>{
+//         console.log("connection Successfully established")
+//     })
+//     .catch(err => console.log(err));
+
+// async function main() {
+//   await mongoose.connect(mongoUrl,{ useNewUrlParser: true, useUnifiedTopology: true });
+// }
+
+// const mongoClientPromise = new Promise((resolve) => {
+//     mongoose.connection.on("connected", () => {
+//         const client = mongoose.connection.getClient();
+//         resolve(client);
+//     });
+// });
+
+
+// app.listen("8080",()=>{
+//     console.log("port is working")
+// });
+
+// // session
+
+// const store =  MongoStore.create({
+//     mongoUrl: mongoUrl,
+//     crypto: {
+//         secret: process.env.SECRET
+//     },
+//     touchafter: 24 * 3600,
+//     clientPromise: mongoClientPromise,
+//     collection : "sessions",
+// });
+
+// store.on("error",(err)=>{
+//     console.log("error in session store",err)
+// });
+
+// const sessionOption = {
+//     store,
+//     secret: process.env.SECRET,
+//     resave: false,
+//     saveUninitialized: true,
+//     cookie: {
+//         expires : Date.now() + 7 * 24 * 60 * 60 * 1000,
+//         maxAge : 7 * 24 * 60 * 60 * 1000,
+//         httpOnly : true
+//     }
+// };
+
+
+// app.use(session(sessionOption));
+
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
 }
+
 const express = require("express");
-const app  = express();
+const app = express();
 const mongoose = require('mongoose');
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressErrors.js");
 
-
 const listingsRoute = require("./routes/listing.js");
 const reviewsRoute = require("./routes/review.js");
+const userRoute = require("./routes/user.js");
 
 const session = require("express-session");
 const MongoStore = require('connect-mongo');
@@ -19,68 +104,69 @@ const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./Models/user.js");
-const userRoute = require("./routes/user.js")
 const mongoUrl = process.env.ATLASDB_URL;
 
-app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"/views"));
-app.use(express.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.engine("ejs",ejsMate);
-app.use(express.static(path.join(__dirname,"/public")));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 main()
-    .then(()=>{
-        console.log("connection Successfully established")
+    .then(() => {
+        console.log("Connection successfully established");
+        startServer();
     })
     .catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect(mongoUrl,{ useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 }
 
 const mongoClientPromise = new Promise((resolve) => {
-    mongoose.connection.on("connected", () => {
-        const client = mongoose.connection.getClient();
-        resolve(client);
+    mongoose.connection.once("open", () => {
+        resolve(mongoose.connection.getClient());
     });
 });
 
+function startServer() {
+    app.listen(8080, () => {
+        console.log("Server is running on port 8080");
+    });
 
-app.listen("8080",()=>{
-    console.log("port is working")
-});
+    // Session setup
+    const store = MongoStore.create({
+        mongoUrl: mongoUrl,
+        crypto: {
+            secret: process.env.SECRET
+        },
+        touchAfter: 24 * 3600,
+        clientPromise: mongoClientPromise,
+        collection: "sessions",
+    });
 
-// session
+    store.on("error", (err) => {
+        console.log("Error in session store", err);
+    });
 
-const store =  MongoStore.create({
-    mongoUrl: mongoUrl,
-    crypto: {
-        secret: process.env.SECRET
-    },
-    touchafter: 24 * 3600,
-    clientPromise: mongoClientPromise,
-    collection : "sessions",
-});
+    const sessionOption = {
+        store,
+        secret: process.env.SECRET,
+        resave: false,
+        saveUninitialized: true,
+        cookie: {
+            expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+            httpOnly: true
+        }
+    };
 
-store.on("error",(err)=>{
-    console.log("error in session store",err)
-});
+    app.use(session(sessionOption));
 
-const sessionOption = {
-    store,
-    secret: process.env.SECRET,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        expires : Date.now() + 7 * 24 * 60 * 60 * 1000,
-        maxAge : 7 * 24 * 60 * 60 * 1000,
-        httpOnly : true
-    }
-};
+    // ... Other middleware and routes ...
+}
 
-
-app.use(session(sessionOption));
 app.use(flash());
 
 app.use(passport.initialize());
